2db4691febf39d2bdd7d9c370f22d1fb5db8adb5
Dominic Aquilina <dijaquil@uwaterloo.ca>
2013-06-14 02:47:43 -0400

    Added makefile and created test file with main

---
+++ make.sh
@@ -0,0 +1,2 @@
+test : util.cpp world.hpp scene.hpp action.hpp pointofinterest.hpp
+  g++ -c test.cpp
---
+++ test.cpp
@@ -0,0 +1,11 @@
+//Includes
+#include "util.cpp"
+#include "world.hpp"
+#include "scene.hpp"
+#include "action.hpp"
+#include "pointofinterest.hpp"
+
+int main ()
+{
+  //Do nothing
+}
